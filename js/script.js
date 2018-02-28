e940dfa982baf2e96c57b0b4219df9cfb6162b5d
Treeek <johngc2010@hotmail.com>
2018-02-28 11:35:15 -0300

    plox funfa

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function add(id){
+    document.getElementById(id).innerHTML =
+    parseInt(document.getElementById(id).innerHTML) + 1;  
+}
+function dec(id){
+    document.getElementById(id).innerHTML =
+    parseInt(document.getElementById(id).innerHTML) - 1;  
+}
